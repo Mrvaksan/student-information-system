cf2d3ff69c9477cfa0ca0e26cce25a8ab7b772bb
Merve Aksan <merveaksan209@gmail.com>
2022-04-25 22:33:28 +0300

    Refactor variable and function.

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,69 @@
 import java.util.Scanner;
 
 public class Main {
+    static double calculateAverage(double grade1, double grade2, double grade3) {
+        return (grade1 + grade2 + grade3) / 3;
+    }
+
+    static String getReward(double ort) {
+        if (ort >= 90) {
+            return "iPHONE";
+        } else if (ort >= 80) {
+            return "iWatch";
+        } else if (ort >= 70) {
+            return "AirPods";
+        } else {
+            return "-";
+        }
+    }
+
+    static void printStudentInfo(String name, String surname, int id, double grade1, double grade2, double grade3) {
+        System.out.printf("%-15s%-15s%-15d%-15.2f%-15.2f%-15.2f%-15.2f%-15s\n", name, surname, id, grade1, grade2, grade3, calculateAverage(grade1, grade2, grade3), getReward(calculateAverage(grade1, grade2, grade3)));
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String isimler[] = new String[5];
-        String soyisimler[] = new String[5];
-        int numaralar[] = new int[5];
-        double not1[] = new double[5];
-        double not2[] = new double[5];
-        double not3[] = new double[5];
-        double ort[] = new double[5];
-        String odul[] = new String[5];
+        String names[] = new String[5];
+        String surnames[] = new String[5];
+        int id[] = new int[5];
+        double grade1[] = new double[5];
+        double grade2[] = new double[5];
+        double grade3[] = new double[5];
+        double average[] = new double[5];
+        String reward[] = new String[5];
 
-        for (int i=0;i<5;i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Ogrenci Bilgilerini Giriniz.");
             System.out.print("Ogrencinin Ismi: ");
-            isimler[i] = scan.next();
+            names[i] = scan.next();
 
-            if(isimler[i].equals("SON")){
+            if (names[i].equals("SON")) {
                 System.out.println("Baska Ogrenci Giremezsiniz!");
                 break;
             }
 
             System.out.print("Ogrencinin Soyismi: ");
-            soyisimler[i] = scan.next();
+            surnames[i] = scan.next();
             System.out.print("Ogrencinin Okul Numarasi: ");
-            numaralar[i] = scan.nextInt();
+            id[i] = scan.nextInt();
             System.out.print("Ogrencinin 1. Notu: ");
-            not1[i] = scan.nextDouble();
-            System.out.print("Ogrencinin 2.Notu: ");
-            not2[i] = scan.nextDouble();
+            grade1[i] = scan.nextDouble();
+            System.out.print("Ogrencinin 2. Notu: ");
+            grade2[i] = scan.nextDouble();
             System.out.print("Ogrencinin 3. Notu: ");
-            not3[i] = scan.nextDouble();
+            grade3[i] = scan.nextDouble();
 
-            ort[i] = ((not1[i] + not2[i] + not3[i]) / 3);
+            average[i] = calculateAverage(grade1[i], grade2[i], grade3[i]);
+            reward[i] = getReward(average[i]);
 
-            if(ort[i] >= 90) {
-                odul[i] = "iPhone";
-            } else if (ort[i] >= 80) {
-                odul[i] = "iWatch";
-            } else if (ort[i] >= 70) {
-                odul[i] = "AirPods";
-            }
-            else {
-                odul[i] = "-";
-            }
         }
+        System.out.printf("%-15s%-15s%-15s%-15s%-15s%-15s%-15s%-15s\n", "ISIM", "SOYISIM", "OGRENCI NO", "NOT 1", "NOT 2", "NOT 3", "ORTALAMA", "ODUL");
 
-        System.out.printf("%-15s%-15s%-15s%-15s%-15s%-15s%-15s%-15s\n","ISIM","SOYISIM","OGRENCI NO","NOT 1","NOT 2","NOT 3","ORTALAMA","ODUL");
-
-        for (int i=0;i<5;i++) {
-            if (isimler[i].equals("SON")) {
+        for (int i = 0; i < 5; i++) {
+            if (names[i].equals("SON")) {
                 break;
             }
-
-            System.out.printf("%-15s%-15s%-15d%-15.2f%-15.2f%-15.2f%-15.2f%-15s\n", isimler[i], soyisimler[i], numaralar[i], not1[i], not2[i], not3[i], ort[i], odul[i]);
+            printStudentInfo(names[i], surnames[i], id[i], grade1[i], grade2[i], grade3[i]);
         }
     }
 }
