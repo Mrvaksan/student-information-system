2859c4c8b934a11578d0730b589f68580fb6a286
Merve Aksan <merveaksan209@gmail.com>
2022-04-26 21:56:07 +0300

    Add feature to grade students

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Student[] students = new Student[5];
+        Teacher teacher=new Teacher();
+        int studentCount=0;
 
         for (int i = 0; i < 5; i++) {
             students[i] = new Student();
+            studentCount++;
 
             System.out.println("Ogrenci Bilgilerini Giriniz.");
             System.out.print("Ogrencinin Ismi: ");
@@ -22,14 +25,11 @@ public class Main {
             students[i].surname = scan.next();
             System.out.print("Ogrencinin Okul Numarasi: ");
             students[i].id = scan.nextInt();
-            System.out.print("Ogrencinin 1. Notu: ");
-            students[i].grade1 = scan.nextDouble();
-            System.out.print("Ogrencinin 2. Notu: ");
-            students[i].grade2 = scan.nextDouble();
-            System.out.print("Ogrencinin 3. Notu: ");
-            students[i].grade3 = scan.nextDouble();
-
         }
+
+        //Teacher will grade students here
+        teacher.gradeStudents(students, studentCount);
+
         System.out.printf("%-15s%-15s%-15s%-15s%-15s%-15s%-15s%-15s\n", "ISIM", "SOYISIM", "OGRENCI NO", "NOT 1", "NOT 2", "NOT 3", "ORTALAMA", "ODUL");
 
         for (int i = 0; i < 5; i++) {
---
+++ src/Teacher.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Teacher {
+    public void gradeStudents(Student[] students, int count){
+        Random random=new Random();
+
+        for (int i = 0; i < count; i++) {
+            students[i].grade1= random.nextDouble(50,101);
+            students[i].grade2= random.nextDouble(50,101);
+            students[i].grade3= random.nextDouble(50,101);
+        }
+    }
+}
